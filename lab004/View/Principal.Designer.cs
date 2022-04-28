fd6e18477ad0c981ff8bd487f289219bce0f6a8a
KAUAN MARTINS CARDOSO <kauan.mcardoso@senacsp.edu.br>
2022-04-28 15:57:20 -0300

    Adicionar arquivos de projeto.

---
+++ lab004/View/Principal.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace lab004.View
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.tbxNum = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(311, 93);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(107, 15);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Digite um número:";
+            this.lblTitulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblResultado.Location = new System.Drawing.Point(311, 188);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(181, 92);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbxNum
+            // 
+            this.tbxNum.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.tbxNum.Location = new System.Drawing.Point(311, 123);
+            this.tbxNum.Name = "tbxNum";
+            this.tbxNum.Size = new System.Drawing.Size(100, 23);
+            this.tbxNum.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(417, 123);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.tbxNum);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblTitulo);
+            this.KeyPreview = true;
+            this.Name = "Principal";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema de calculo quadrado";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Principal_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox tbxNum;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
---
+++ lab004/View/Principal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab004.View
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tbxNum.Text == "")
+            {
+                MessageBox.Show("Digite um número");
+            }
+            else
+            {
+                int numero = 0;
+                int quadrado = 0;
+
+                numero = Convert.ToInt32(tbxNum.Text);
+
+                quadrado = numero * numero;
+
+                lblResultado.Text = "O número " + numero + " ao quadrado é " + quadrado.ToString();
+
+                tbxNum.Focus();
+                tbxNum.SelectAll();
+            }
+
+        }
+
+        private void Principal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            if(e.KeyChar == 13)
+            {
+                btnCalcular.PerformClick();
+            }
+        }
+    }
+}
